5efa64daf384aace01ddf1bfcb7cc5ecf81c8060
Felipe Freire <fellipefreiire3@gmail.com>
2021-04-23 15:01:16 -0300

    teste1

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,124 +1,130 @@
-import { GetStaticPaths, GetStaticProps } from 'next'
-import Image from 'next/image'
-import Link from 'next/link'
-import Head from 'next/head'
-
-import { api } from '../../services/api'
-
-import { format, parseISO } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
-import { convertDurationToTimeString } from '../../../utils/convertDurationToTimeString'
-
-import * as S from '../../styles/episodes'
-import { usePlayer } from '../../contexts/PlayerContext'
-
-type Episode = {
-  id: string
-  title: string
-  thumbnail: string
-  members: string
-  publishedAt: string
-  duration: number
-  durationAsString: string
-  description: string
-  url: string
+const Teste1 = () => {
+  return <div></div>
 }
 
-type EpisodeProps = {
-  episode: Episode
-}
+export default Teste1
 
-const Episode: React.FC<EpisodeProps> = ({ episode }): JSX.Element => {
-  const { play } = usePlayer()
+// import { GetStaticPaths, GetStaticProps } from 'next'
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import Head from 'next/head'
 
-  return (
-    <S.Episode>
-      <Head>
-        <title>{episode.title} | Podcastr</title>
-      </Head>
-      <S.ThumbnailContainer>
-        <Link href='/'>
-          <button type='button'>
-            <img src='/arrow-left.svg' alt='Voltar' />
-          </button>
-        </Link>
-        <Image
-          width={700}
-          height={160}
-          src={episode.thumbnail}
-          objectFit='cover'
-        />
-        <button
-          type='button'
-          onClick={() => {
-            play(episode)
-          }}
-        >
-          <img src='/play.svg' alt='Tocar Episódio' />
-        </button>
-      </S.ThumbnailContainer>
+// import { api } from '../../services/api'
 
-      <header>
-        <h1>{episode.title}</h1>
-        <span>{episode.members}</span>
-        <span>{episode.publishedAt}</span>
-        <span>{episode.durationAsString}</span>
-      </header>
+// import { format, parseISO } from 'date-fns'
+// import ptBR from 'date-fns/locale/pt-BR'
+// import { convertDurationToTimeString } from '../../../utils/convertDurationToTimeString'
 
-      <S.Description
-        dangerouslySetInnerHTML={{ __html: episode.description }}
-      />
-    </S.Episode>
-  )
-}
+// import * as S from '../../styles/episodes'
+// import { usePlayer } from '../../contexts/PlayerContext'
 
-export default Episode
+// type Episode = {
+//   id: string
+//   title: string
+//   thumbnail: string
+//   members: string
+//   publishedAt: string
+//   duration: number
+//   durationAsString: string
+//   description: string
+//   url: string
+// }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const { data } = await api.get('episodes', {
-    params: {
-      _limit: 2,
-      _sort: 'published_at',
-      _order: 'desc'
-    }
-  })
+// type EpisodeProps = {
+//   episode: Episode
+// }
 
-  const paths = data.map(episode => {
-    return {
-      params: {
-        slug: episode.id
-      }
-    }
-  })
+// const Episode: React.FC<EpisodeProps> = ({ episode }): JSX.Element => {
+//   const { play } = usePlayer()
 
-  return {
-    paths,
-    fallback: 'blocking'
-  }
-}
+//   return (
+//     <S.Episode>
+//       <Head>
+//         <title>{episode.title} | Podcastr</title>
+//       </Head>
+//       <S.ThumbnailContainer>
+//         <Link href='/'>
+//           <button type='button'>
+//             <img src='/arrow-left.svg' alt='Voltar' />
+//           </button>
+//         </Link>
+//         <Image
+//           width={700}
+//           height={160}
+//           src={episode.thumbnail}
+//           objectFit='cover'
+//         />
+//         <button
+//           type='button'
+//           onClick={() => {
+//             play(episode)
+//           }}
+//         >
+//           <img src='/play.svg' alt='Tocar Episódio' />
+//         </button>
+//       </S.ThumbnailContainer>
 
-export const getStaticProps: GetStaticProps = async ctx => {
-  const { slug } = ctx.params
-  const { data } = await api.get(`/episodes/${slug}`)
+//       <header>
+//         <h1>{episode.title}</h1>
+//         <span>{episode.members}</span>
+//         <span>{episode.publishedAt}</span>
+//         <span>{episode.durationAsString}</span>
+//       </header>
 
-  const episode = {
-    id: data.id,
-    title: data.title,
-    thumbnail: data.thumbnail,
-    members: data.members,
-    publishedAt: format(parseISO(data.published_at), 'd MMM yy', {
-      locale: ptBR
-    }),
-    duration: Number(data.file.duration),
-    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
-    description: data.description,
-    url: data.file.url
-  }
+//       <S.Description
+//         dangerouslySetInnerHTML={{ __html: episode.description }}
+//       />
+//     </S.Episode>
+//   )
+// }
 
-  return {
-    props: {
-      episode
-    },
-    revalidate: 60 * 60 * 24 // 24 hours
-  }
-}
+// export default Episode
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const { data } = await api.get('api/episodes', {
+//     params: {
+//       _limit: 2,
+//       _sort: 'published_at',
+//       _order: 'desc'
+//     }
+//   })
+
+//   const paths = data.map(episode => {
+//     return {
+//       params: {
+//         slug: episode.id
+//       }
+//     }
+//   })
+
+//   return {
+//     paths,
+//     fallback: 'blocking'
+//   }
+// }
+
+// export const getStaticProps: GetStaticProps = async ctx => {
+//   const { slug } = ctx.params
+//   const { data } = await api.get(`/episodes/${slug}`)
+
+//   const episode = {
+//     id: data.id,
+//     title: data.title,
+//     thumbnail: data.thumbnail,
+//     members: data.members,
+//     publishedAt: format(parseISO(data.published_at), 'd MMM yy', {
+//       locale: ptBR
+//     }),
+//     duration: Number(data.file.duration),
+//     durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+//     description: data.description,
+//     url: data.file.url
+//   }
+
+//   return {
+//     props: {
+//       episode
+//     },
+//     revalidate: 60 * 60 * 24 // 24 hours
+//   }
+// }
