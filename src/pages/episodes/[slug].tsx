a77aee08e76a572c9c840def4179b9d1e0b097fe
Felipe Freire <fellipefreiire3@gmail.com>
2021-04-23 16:39:36 -0300

    import fix

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Head from 'next/head'
 
-import { api, api2 } from '../../services/api'
+import { api } from '../../services/api'
 
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
